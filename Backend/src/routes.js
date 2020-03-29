e70361bcfdf2932833bb608f7c4b58ef8dfd7a40
joaovitorbatistella <joaovitor.batistella.ifrs@gmail.com>
2020-03-29 20:30:08 -0300

    all

---
+++ Backend/src/routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentsController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+
+const routes = express.Router();
+
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(12).max(13),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+}), ProfileController.index);
+
+routes.post('/incidents', IncidentsController.create);
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), IncidentsController.index);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentsController.delete);
+
+module.exports = routes;
---
+++ Backend/teoria.js
@@ -0,0 +1,30 @@
+/**
+ * Rota / Recursos
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET: Buscar/listar uma informação no back-end;
+  * POST: Criar uma informação no back-end;
+  * PUT: Alterar um ainformação no back-end;
+  * DELETE: Deletar uma informação no back-end;
+  */
+
+  /**
+   * Tipos de parâmetros:
+   * 
+   * Query Params: Parâmetos nomeados enviados na rota após "?" (Filtros, paginação);
+   * Route Params: Parâmetros utilizados para identificr recursos;
+   * Request Boddy: Corpo da requiseção, utilizado para criar ou alterar recursos;
+   */
+
+   /**
+    * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server;
+    * NoSQL: MongoDB, CoucDB, etc;
+    */
+
+    /**
+     * Driver: SELECT * FROM users;
+     * Querry Builder: table('users').select('*').where();
+     */
